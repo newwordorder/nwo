18fd3a39c3cce60cd350956b21a01f762905271c
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-03-08 13:49:10 +1000

    Updated some styles

--- wp-content/themes/nwo/page-templates/blocks/50-50.php
+++ wp-content/themes/nwo/page-templates/blocks/50-50.php
@@ -1,28 +1,19 @@
 <?php  // 50 50 SPLIT BLOCK
-
 if (get_row_layout() == '50_50') :
-
-  $text = get_sub_field('text_block');
-  $image = get_sub_field('image');
-  $flipLayout = get_sub_field('flip_layout');
-
-
-  ?>
-
+    $text = get_sub_field('text_block');
+    $image = get_sub_field('image');
+    $flipLayout = get_sub_field('flip_layout');
+    ?>
 <div class="imageblock switchable <?php if (!($flipLayout) == 'yes') : echo 'switchable--switch';
-                                  endif; ?>">
-
+                                    endif; ?>">
     <div class="imageblock__content col-lg-6 pos-right">
         <div class="background-image-holder">
             <?php
-
             if (!empty($image)) :
-
-              // vars
-              $url = $image['url'];
-              $alt = $image['alt'];
-
-              ?>
+                // vars
+                $url = $image['url'];
+                $alt = $image['alt'];
+                ?>
             <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>" />
             <?php endif; ?>
         </div>
@@ -37,7 +28,5 @@ if (get_row_layout() == '50_50') :
         </div>
     </div>
 </div>
-
 <?php endif;
-
 ?> 
