06c79b18348e9d3c40da9b41a539308ddb056899
Elliot Kidd <elliot.kidd89@gmail.com>
2019-03-13 16:51:23 +1000

    clean out js

--- wp-content/themes/nwo/footer.php
+++ wp-content/themes/nwo/footer.php
@@ -47,9 +47,6 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/flickity.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/fontawesome-all.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/parallax.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/typed.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/smooth-scroll.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/ytplayer.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/aos.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/map.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
